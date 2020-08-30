9dfb8dc27a8469f108eeecb2faea39859d7dc750
afozbek <furkanozbek1995@gmail.com>
2020-08-30 15:08:29 +0300

    popup-conten arasında iletişim sağlandı

--- content.js
+++ content.js
@@ -1,1 +1,13 @@
 chrome.runtime.sendMessage({ todo: "showPageAction" });
+
+chrome.runtime.onMessage.addListener(function (request, sender, response) {
+  switch (request.todo) {
+    case "changeColor":
+      let addColor = "#" + request.clickedColor;
+
+      $(".api").css("color", addColor);
+      break;
+    default:
+      return;
+  }
+});
